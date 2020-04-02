f273917ad027de298a9d05ee2f85648559f4e79c
gannagoodkevich <gannagoodkevich@gmail.com>
2020-04-02 13:03:20 +0300

    Create Book#new with jquery

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,5 +1,19 @@
 class BooksController < ApplicationController
+  def new
+    @book = Book.new
+  end
+
   def index
     @books = Book.all
+  end
+
+  def create
+    @book = Book.create!(book_params)
+  end
+
+  private
+
+  def book_params
+    params.require(:book).permit(:title, :author)
   end
 end
