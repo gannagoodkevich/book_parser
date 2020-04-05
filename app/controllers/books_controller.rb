4a556deed78f43b1d81b8b76944e9b8e2ed2b0ff
gannagoodkevich <gannagoodkevich@gmail.com>
2020-04-05 16:13:27 +0300

    Add Cover and Status cloud

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -2,26 +2,46 @@ class BooksController < ApplicationController
   def new
     @book = Book.new
     @genres = Genre.all
+    @covers = Cover.all
+    @statuses = Status.all
   end
 
   def index
+    @books = Book.all.order(title: :asc)
+
     if params[:genre_id]
       @genre = Genre.find_by(id: params[:genre_id])
       @books = @genre.books
-    else
-      if params[:order]
-        @books = Book.order(title: :desc)
-      else
-        @books = Book.order(title: :asc)
-      end
     end
+
+    if params[:status_id]
+      @status = Status.find_by(id: params[:status_id])
+      puts @status
+      @books = @status.books
+    end
+
+    if params[:cover_id]
+      @cover = Cover.find_by(id: params[:cover_id])
+      @books = @cover.books
+    end
+
+    if params[:order]
+      @books = @books.includes(:cover).order('covers.cover_type ASC')
+    end
+
     @genres = Genre.all
+    @covers = Cover.all
+    @statuses = Status.all
   end
 
   def create
     @genre = Genre.find_by(id: params[:book][:genre_id])
     puts @genre
     @book = @genre.books.create!(book_params)
+    @status = Status.find_by(id: params[:book][:status_id])
+    @status.books << @book
+    @cover = Cover.find_by(id: params[:book][:cover_id])
+    @cover.books << @book
     respond_to do |format|
       format.js
     end
@@ -30,6 +50,8 @@ class BooksController < ApplicationController
   def edit
     @book = Book.find_by(id: params[:id])
     @genres = Genre.all
+    @covers = Cover.all
+    @statuses = Status.all
   end
 
   def update
@@ -57,6 +79,6 @@ class BooksController < ApplicationController
   private
 
   def book_params
-    params.require(:book).permit(:title, :author, :genre_id)
+    params.require(:book).permit(:title, :author, :genre_id, :cover_id, :status_id)
   end
 end
--- db/migrate/20200331200216_create_covers.rb
+++ db/migrate/20200331200216_create_covers.rb
@@ -1,7 +1,7 @@
 class CreateCovers < ActiveRecord::Migration[6.0]
   def change
     create_table :covers do |t|
-      t.string :type
+      t.string :cover_type
       t.integer :book_id
 
       t.timestamps
