586fcbbb07fa6e9e646bb50e0387605ff848adb5
gannagoodkevich <gannagoodkevich@gmail.com>
2020-04-13 12:35:34 +0300

    Finale fixes

--- app/servises/book_filter.rb
+++ app/servises/book_filter.rb
@@ -1,11 +1,12 @@
 class BookFilter
   def call(params)
-    @book_attribute = Genre.find_by(id: params[:genre_id]) if params[:genre_id]
+    book_attribute = Genre.find_by(id: params[:genre_id]) if params[:genre_id]
 
-    @book_attribute = Status.find_by(id: params[:status_id]) if params[:status_id]
+    book_attribute = Status.find_by(id: params[:status_id]) if params[:status_id]
 
-    @book_attribute = Cover.find_by(id: params[:cover_id]) if params[:cover_id]
+    book_attribute = Cover.find_by(id: params[:cover_id]) if params[:cover_id]
 
-    @book_attribute&.books
+    puts book_attribute&.books.inspect
+    book_attribute&.books
   end
 end
